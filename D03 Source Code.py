7c6db88396757cddb204d45c7f422967ceb228e6
Lucas Guzman <lucas_guzman@hotmail.es>
2024-04-22 17:20:06 +0200

    nada Co-authored-by: olayaa03 <olayaa03@users.noreply.github.com>

--- D03 Source Code.py
+++ D03 Source Code.py
@@ -1,18 +1,34 @@
 from MFIS_Classes import *
 from MFIS_Read_Functions import *
+import skfuzzy as fuzz
 
 # read all information from files
-fuzzySetsDict = readFuzzySetsFile("Files\InputVarSets.txt") # correct file??
+fuzzySetsDict = readFuzzySetsFile("Files\InputVarSets.txt")  # correct file??
 rules = readRulesFile()
 applicationList = readApplicationsFile()
 
 
-print("*** Fuzzy sets dict ***")
-fuzzySetsDict.printFuzzySetsDict()
+# print("*** Fuzzy sets dict ***")
+# fuzzySetsDict.printFuzzySetsDict()
 
-print("*** Rule list ***")
-rules.printRuleList()
+# print("*** Rule list ***")
+# rules.printRuleList()
 
-print("*** Application list ***")
-for x in applicationList:
-    x.printApplication()
+# print("*** Application list ***")
+# for x in applicationList:
+#     x.printApplication()
+
+# for app in applicationList:
+# obtain memdegree for each fuzzyset
+"""     inputDict = {
+    str(key): val for key, val in applicationList[0].data
+}  # {Age: 34, IncomeLeve: 73...} """
+inputDict = {"Age": 35, "IncomeLevel": 38, "Assets": 19,"Amount": 8, "Job": 0, "History": 1}
+for setid in fuzzySetsDict:
+    fuzzySetsDict[setid].memDegree = fuzz.interp_membership(
+        fuzzySetsDict[setid].x,
+        fuzzySetsDict[setid].y,
+        inputDict[fuzzySetsDict[setid].var],
+    )
+    fuzzySetsDict[setid].printSet()
+
